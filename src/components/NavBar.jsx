7ad7a3210198ab1dd685f66c909e711209c46988
acostajluism <acostajluism@gmail.com>
2024-08-19 14:55:11 -0300

    Agrego Navbar, CartWidget, e ItemListContainer con routing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CartWidget from './CartWidget';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Mi Tienda</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/electronica">Electrónica</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/ropa">Ropa</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/accesorios">Accesorios</Link>
+            </li>
+          </ul>
+        </div>
+        <CartWidget />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
